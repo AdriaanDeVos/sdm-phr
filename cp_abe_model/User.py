664cdf2c4c070ef9c763aab37c7e9bc5f6682f09
DMoonen <Jackos124@gmail.com>
2021-10-25 13:12:37 +0200

    Add User documentation

--- cp_abe_model/User.py
+++ cp_abe_model/User.py
@@ -9,15 +9,24 @@ class UserClass:
     cpabe = BSW07(pairing_group, 2)
 
     def __init__(self, user_id, role, ta, fs):
+        """
+            Initialize the User object
+            :param user_id: An identifier for the user.
+            :param role: An enum for the role of theuser.
+            :param ta: The trusted authority the user.
+            :param fs: The fileserver of the user.
+        """
         self.__user_id = user_id
         self.__role = role
         self.__ta = ta
         self.__user_key = ta.key_request(user_id)
         self.__file_server = fs
 
+    # TODO
     def __encrypt_message(self):
         return
 
+    # TODO
     def __decrypt_message(self):
         return
 
@@ -34,7 +43,15 @@ class UserClass:
         return
 
     def get_user_id(self):
+        """
+            Get user_id.
+            :return: An int representing the id of the user.
+        """
         return self.__user_id
 
     def get_role(self):
+        """
+            Get role.
+            :return: An enum representing the role of the user.
+        """
         return self.__role
