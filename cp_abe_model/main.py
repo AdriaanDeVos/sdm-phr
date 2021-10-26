fb56b18a445ae713c2bd51ae39bc2663edf6191a
DMoonen <Jackos124@gmail.com>
2021-10-26 15:02:23 +0200

    ADD User creation in main

--- cp_abe_model/main.py
+++ cp_abe_model/main.py
@@ -6,21 +6,26 @@ from Role import ROLE
 def main():
     print("Starting main program...")
     user_object_list = []
-    user_role_amount = [2, 3, 2, 0, 0, 0]
+    user_role_amount = [2, 3, 2, 0, 4, 0]
     user_list = {0: ['ONE', 'TWO', 'THREE']}
     attr_list = ['ONE', 'TWO', 'THREE', 'FOUR']
     ta = TAClass(attr_list, user_list)
     # TODO init FS
     fs = 0
 
-    # TODO initialize users based on the user_role_amount list
-    for i in range(6):
-        for amount in range(len(user_role_amount, user_role_amount+ user_role_amount[i], 1)):
-            print(str(i) + " " + str(amount))
-            #user_object_list.append(UserClass(0, ROLE(0), ta, fs))
+    for i in range(len(user_role_amount)):
+        user_object_list = user_object_list + make_users(len(user_object_list), ROLE(i), ta, fs, user_role_amount[i])
 
-    # TODO test that users have correct roles/ids
-    
+    for user in user_object_list:
+        print(str(user.get_user_id()) + " " + str(user.get_role()))
+
+
+def make_users(user_id, role, ta, fs, amount):
+    result = []
+    for i in range(amount):
+        result.append(UserClass(user_id + i, role, ta, fs))
+    return result
+
 
 if __name__ == "__main__":
     main()
