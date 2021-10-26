03718c44081ab3a5df649314bfe2bf6cc6e96776
DMoonen <Jackos124@gmail.com>
2021-10-26 16:19:58 +0200

    Finish user_test

--- cp_abe_model/User_Test.py
+++ cp_abe_model/User_Test.py
@@ -1,28 +1,27 @@
 from User import UserClass
 from TA import TAClass
 from Role import ROLE
+from phr_repo import PHRRepo
 
 
 def main():
     print("Starting user test...")
-    print(ROLE(0))
     user_object_list = []
     user_list = {0: ['ONE', 'TWO', 'THREE']}
     attr_list = ['ONE', 'TWO', 'THREE', 'FOUR']
     ta = TAClass(attr_list, user_list)
-    # TODO init FS
-    fs = 0
+    fs = PHRRepo()
 
     user_object_list.append(UserClass(0, ROLE(0), ta, fs))
 
     user = user_object_list[0]
-    print(user.get_user_id())
-    print(user.get_role())
-    # TODO
-    """
-    encrypt_and_send(self, message, policy)
-    decrypt_and_send(self, message_id)
-    """
+
+    policy = '((ONE and THREE) and (TWO OR FOUR))'
+    message = "dekatkraptdekrullenvandetrap"
+    file_id = user.encrypt_and_send(user.get_user_id(), message, policy)
+    print(file_id)
+
+    print("Successful decryption:", str(user.decrypt_from_send(file_id) == message))
 
 
 if __name__ == "__main__":
