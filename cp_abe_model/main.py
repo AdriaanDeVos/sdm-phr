934e0481892b8b6a850f6985b7e11bc733571fb6
DMoonen <Jackos124@gmail.com>
2021-10-26 16:20:04 +0200

    Finish main

--- cp_abe_model/main.py
+++ cp_abe_model/main.py
@@ -1,6 +1,7 @@
 from User import UserClass
 from TA import TAClass
 from Role import ROLE
+from phr_repo import PHRRepo
 
 
 def main():
@@ -10,14 +11,22 @@ def main():
     user_list = {0: ['ONE', 'TWO', 'THREE']}
     attr_list = ['ONE', 'TWO', 'THREE', 'FOUR']
     ta = TAClass(attr_list, user_list)
-    # TODO init FS
-    fs = 0
+    fs = PHRRepo()
 
     for i in range(len(user_role_amount)):
         user_object_list = user_object_list + make_users(len(user_object_list), ROLE(i), ta, fs, user_role_amount[i])
 
-    for user in user_object_list:
-        print(str(user.get_user_id()) + " " + str(user.get_role()))
+    #for user in user_object_list:
+    #    print(str(user.get_user_id()) + " " + str(user.get_role()))
+
+    policy = '((ONE and THREE) and (TWO OR FOUR))'
+    message = "dekatkraptdekrullenvandetrap"
+    print("Encrypting file...")
+    file_id = user_object_list[0].encrypt_and_send(user_object_list[0].get_user_id(), message, policy)
+    print("Decrypting file...")
+    decr_message = user_object_list[0].decrypt_from_send(file_id)
+
+    print("Successful decryption:", str(decr_message == message))
 
 
 def make_users(user_id, role, ta, fs, amount):
